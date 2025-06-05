217f1a92a62c15b9c0c537a74861221cb60ef3bc
Prasanna Lakshmi <167883557+prasanna-lakshmi18@users.noreply.github.com>
2025-06-05 21:04:32 +0530

    Delete narrator directory

--- narrator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, narrate_story
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('narrate/', narrate_story, name='narrate_story'),
-]
